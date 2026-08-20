af538f52bf340002bf082c15044174a080c37c71
xiaomeng9597 <519561910@qq.com>
2026-08-20 17:35:46 +0800

    更新18

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -7,6 +7,16 @@
 # Blog: https://p3terx.com
 #===============================================
 
+# 修改uhttpd配置文件，启用nginx
+# sed -i "/.*uhttpd.*/d" .config
+# sed -i '/.*\/etc\/init.d.*/d' package/network/services/uhttpd/Makefile
+# sed -i '/.*.\/files\/uhttpd.init.*/d' package/network/services/uhttpd/Makefile
+sed -i "s/:80/:81/g" package/network/services/uhttpd/files/uhttpd.config
+sed -i "s/:443/:4443/g" package/network/services/uhttpd/files/uhttpd.config
+cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
+# ls package/base-files/files/etc/
+
+
 # 追加自定义内核配置项
 echo "CONFIG_PSI=y
 CONFIG_KPROBES=y
